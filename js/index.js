aff1172f39983833242304a303356773c489f650
arafatanjan <arafatanjan@gmail.com>
2022-04-16 11:36:19 +0600

    first comoit

--- js/index.js
+++ js/index.js
@@ -3,21 +3,23 @@ const searchFood = () => {
     const searchText = searchField.value;
     // console.log(searchText);
     searchField.value = '';
+    // searchText = = '';
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     //console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.meals));
+    // displaySearchResult
 }
 
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
                 <img src="${meal.strMealThumb}" class="card-img-top w-50 mx-auto" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
@@ -44,7 +46,7 @@ const displayMealDetail = meal => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
-    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <img src="${meal.strMealThumb}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
